1ea5ae24df9c71933f548db474611c902cd6c9a2
1Rendes <1admin@ukr.net>
2024-09-14 14:32:39 +0200

    task done

--- src/components/SelectRates/SelectRates.jsx
+++ src/components/SelectRates/SelectRates.jsx
@@ -1,19 +1,28 @@
 import Select from 'react-select';
 
-// import symbols from './symbols.json';
+import symbols from './symbols.json';
 
 import styles from './SelectRates.module.css';
 
 import './ReactSelect.css';
+import { useDispatch } from 'react-redux';
+import { setBaseCurrency } from '../../redux/currency/currencySlice';
 
-export const SelectRates = () => {
+export const SelectRates = ({ baseCurrency }) => {
+  const dispatch = useDispatch();
+  const handleChange = selectedOption => {
+    dispatch(setBaseCurrency(selectedOption.value));
+  };
   return (
     <div className={styles.box}>
       <p className={styles.text}>Your base currency:&nbsp;</p>
       <Select
+        onChange={handleChange}
         className={styles.select}
         classNamePrefix="react-select"
         isSearchable
+        options={symbols}
+        value={{ label: baseCurrency, value: baseCurrency }}
       />
     </div>
   );
